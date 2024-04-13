93f5f45b0e8702c37741c2d0d411abdeb0e62125
Maria Alessandra <marialessandramejianavarro@gmail.com>
2024-04-13 17:05:43 -0600

    Algunos errores corregidos

--- app/_components/ProductList.jsx
+++ app/_components/ProductList.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ProductItem from './ProductItem'
 
-function ProductList(productList) {
+function ProductList({productList}) {
   return (
     <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3'>
         {productList.map((item, index)=>index<=3&&(
--- app/_utils/GlobalApi.js
+++ app/_utils/GlobalApi.js
@@ -6,10 +6,10 @@ const { headers } = require("next/headers");
 const apiKey = process.env.NEXT_PUBLIC_REST_API_KEY
 const apiUrl= 'http://localhost:1337/api';
 
-const axiosClient=anxios.create({
+const axiosClient=axios.create({
     baseURL:apiUrl,
     headers:{
-        Authorization:`Bearer ${apikey}`
+        Authorization:`Bearer ${apiKey}`
     }
 })
 
